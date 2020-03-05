8447983ac25d0c2c0d20ba979e53a254ad0566fb
Kristof Menyhert <menyhert.kristof@gmail.com>
2020-03-06 00:45:20 +0100

    Add files via upload

---
+++ corona.R
@@ -0,0 +1,103 @@
+library(data.table)
+library(tidyr)
+library(lubridate)
+library(ggplot2)
+library(ggpubr)
+
+fromwidetolong <- function(link, coltoname){
+  
+  data_wide <- fread(link)
+  long_number <- match("Long",names(data_wide))
+  last <- length(colnames(data_wide))
+  
+  data <- gather(data_wide, date, number, (long_number + 1):last)
+  setDT(data)
+  
+  data[, date:= mdy(date)]
+  
+  setnames(data, old = "number", new = coltoname)
+  
+  return(data)
+  
+}
+
+
+combinetreetable <- function(){
+  cases_t <- fromwidetolong("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv",
+                        "cases")
+  death_t <- fromwidetolong("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv",
+                        "death")
+  recovered_t <- fromwidetolong("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv",
+                            coltoname = "recovered")
+  data <- merge(cases_t, death_t[, .(`Province/State`, `Country/Region`, date, death)], by = c("Province/State", "Country/Region", "date"), all.x = T)
+  data <- merge(data, recovered_t[, .(`Province/State`, `Country/Region`, date, recovered)], by = c("Province/State", "Country/Region", "date"), all.x = T)
+  
+  long_number <- match("Long",names(data))
+  last <- length(colnames(data))
+  
+  data <- gather(data, type, val, (long_number+1):last)
+  
+  setDT(data)
+  
+  setnames(data, old = "Province/State", new = "Provance_State")
+  setnames(data, old = "Country/Region", new = "Country_Region")
+  
+  return(data)
+  
+}
+
+
+
+data <- combinetreetable()
+data_agg <- data[, .(val = sum(val, na.rm = T)), by = .(type, date)]
+
+ggplot(data_agg, aes(date, val)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(type ~ ., scales = "free_y") +
+  theme_bw()
+
+
+
+
+
+cases_death_recovered <- function(count, setScales = "fixed"){
+  
+  data_agg <- data[, .(val = sum(val, na.rm = T)), by = .(type, date, Country_Region)]
+  
+  countries <- count
+  
+  ggplot(data_agg[Country_Region %in% countries & type == "cases"], aes(date, val)) +
+    geom_point() +
+    geom_line() +
+    facet_grid(Country_Region ~ ., scales = setScales) +
+    labs(title = "cases") +
+    theme_bw() -> plot1
+  
+  
+  
+  ggplot(data_agg[Country_Region %in% countries & type == "death"], aes(date, val)) +
+    geom_point() +
+    geom_line() +
+    facet_grid(Country_Region ~ ., scales = setScales) +
+    labs(title = "death") +
+    theme_bw() -> plot2
+  
+  
+  
+  ggplot(data_agg[Country_Region %in% countries & type == "recovered"], aes(date, val)) +
+    geom_point() +
+    geom_line() +
+    facet_grid(Country_Region ~ ., scales = setScales) +
+    labs(title = "recovered") +
+    theme_bw() -> plot3
+  
+  figure <- ggarrange(plot1, plot2, plot3, ncol = 3, nrow = 1)
+  return(figure)
+  
+}
+
+countries <- c("France", "Japan", "Germany", "Italy", "Hungary", "Netherlands", "India")
+
+cases_death_recovered(count = countries)
+cases_death_recovered(count = countries, setScales = "free_y")
